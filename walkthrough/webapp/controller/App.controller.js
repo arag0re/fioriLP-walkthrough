6e2bf381e26145b042ab2d495e06ce017ebb8f6c
arag0re.eth <anton.stadie@icloud.com>
2022-02-07 13:59:29 +0100

    added code for step 4, 5 and 6

--- walkthrough/webapp/controller/App.controller.js
+++ walkthrough/webapp/controller/App.controller.js
@@ -1,15 +1,32 @@
-sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-],
-    /**
-     * @param {typeof sap.ui.core.mvc.Controller} Controller
-     */
-    function (Controller) {
-        "use strict";
+sap.ui.define(
+   [
+      'sap/ui/core/mvc/Controller',
+      'sap/m/MessageToast',
+      'sap/ui/model/json/JSONModel',
+   ],
+   /**
+    * @param {typeof sap.ui.core.mvc.Controller} Controller
+    * @param {typeof sap.m.MessageToast} MessageToast
+    * @param {typeof sap.ui.model.json.JSONModel} JSONModel
+    */
+   function (Controller, MessageToast, JSONModel) {
+      'use strict'
 
-        return Controller.extend("de.icontec.antonstadie.walkthrough.controller.App", {
+      return Controller.extend(
+         'de.icontec.antonstadie.walkthrough.controller.App',
+         {
             onInit: function () {
-
-            }
-        });
-    });
+               var myModel = {
+                  recipient: {
+                     name: 'World',
+                  },
+               }
+            },
+            onShowHello: function () {
+               MessageToast.show('Hello World')
+               //alert('Hello World')
+            },
+         }
+      )
+   }
+)
