12af3e49d3c3740ede07f75f6f1c22ba3ab7e644
arag0re.eth <anton.stadie@icloud.com>
2022-02-07 16:03:55 +0100

    added code for step 7,8 and 9

--- walkthrough/webapp/controller/App.controller.js
+++ walkthrough/webapp/controller/App.controller.js
@@ -3,27 +3,43 @@ sap.ui.define(
       'sap/ui/core/mvc/Controller',
       'sap/m/MessageToast',
       'sap/ui/model/json/JSONModel',
+      'sap/ui/model/resource/ResourceModel',
    ],
    /**
     * @param {typeof sap.ui.core.mvc.Controller} Controller
     * @param {typeof sap.m.MessageToast} MessageToast
     * @param {typeof sap.ui.model.json.JSONModel} JSONModel
+    * @param {typeof sap.ui.model.resource.ResourceModel} ResourceModel
     */
-   function (Controller, MessageToast, JSONModel) {
+   function (Controller, MessageToast, JSONModel, ResourceModel) {
       'use strict'
 
       return Controller.extend(
          'de.icontec.antonstadie.walkthrough.controller.App',
          {
             onInit: function () {
-               var myModel = {
+               var myData = {
                   recipient: {
                      name: 'World',
                   },
                }
+               var myModel = new JSONModel(myData)
+               this.getView().setModel(myModel)
+               var i18nModel = new ResourceModel({
+                  bundleName: 'de.icontec.antonstadie.walkthrough.i18n.i18n',
+               })
+               this.getView().setModel(i18nModel, 'i18n')
             },
             onShowHello: function () {
-               MessageToast.show('Hello World')
+               var i18nBundle = this.getView()
+                  .getModel('i18n')
+                  .getResourceBundle()
+               var modelRecipient = this.getView()
+                  .getModel()
+                  .getProperty('/recipient/name')
+
+               var modelMsg = i18nBundle.getText('helloMsg', [modelRecipient])
+               MessageToast.show(modelMsg)
                //alert('Hello World')
             },
          }
