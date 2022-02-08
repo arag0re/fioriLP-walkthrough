3a24724207680e26d25dab6830b994d8356684f2
arag0re.eth <anton.stadie@icloud.com>
2022-02-08 10:01:32 +0100

    added ComponentConfiguration to index.js

--- walkthrough/webapp/index.js
+++ walkthrough/webapp/index.js
@@ -1,9 +1,13 @@
-sap.ui.define(['sap/ui/core/mvc/XMLView'], function (XMLView) {
-   'use strict'
-
-   XMLView.create({
-      viewName: 'sap.ui.demo.walkthrough.view.App',
-   }).then(function (oView) {
-      oView.placeAt('content')
-   })
-})
+sap.ui.define(
+   ['sap/ui/core/ComponentContainer'],
+   function (ComponentContainer) {
+      'use strict'
+      new ComponentContainer({
+         name: 'de.icontec.as.walkthrough',
+         settings: {
+            id: 'walkthrough',
+         },
+         async: 'true',
+      }).placeAt('content')
+   }
+)
