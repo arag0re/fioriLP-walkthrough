aa3134ce9fb874836a5340a7c68971cd65dbdd82
arag0re.eth <anton.stadie@icloud.com>
2022-02-09 12:48:30 +0100

    added search and sorting to the listView

--- walkthrough/webapp/controller/InvoiceList.controller.js
+++ walkthrough/webapp/controller/InvoiceList.controller.js
@@ -2,9 +2,11 @@ sap.ui.define(
    [
       'sap/ui/core/mvc/Controller',
       'sap/ui/model/json/JSONModel',
+      'sap/ui/model/Filter',
+      'sap/ui/model/FilterOperator',
       '../model/formatter',
    ],
-   function (Controller, JSONModel, formatter) {
+   function (Controller, JSONModel, Filter, FilterOperator, formatter) {
       'use strict'
 
       return Controller.extend(
@@ -12,10 +14,25 @@ sap.ui.define(
          {
             formatter: formatter,
             onInit: function () {
-               var oViewModel = new JSONModel({
+               var currencyModel = new JSONModel({
                   currency: 'EUR',
                })
-               this.getView().setModel(oViewModel, 'view')
+               this.getView().setModel(currencyModel, 'view')
+            },
+            onFilterInvoices: function (oEvent) {
+               var aFilter = []
+               var sQuery = oEvent.getParameter('query')
+               if (sQuery) {
+                  aFilter.push(
+                     new Filter('ProductName', FilterOperator.Contains, sQuery)
+                  )
+               }
+               var oList = this.byId('invoiceList')
+               var oBinding = oList.getBinding('items')
+               oBinding.filter(aFilter)
+            },
+            onChangerOrderButtonPress: function () {
+               this.byId('invoiceList')
             },
          }
       )
